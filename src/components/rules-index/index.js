aa6a3de90187eec262c2fadac3b02efdcbf60ac4
CartoonOne <CartoonOne@users.noreply.github.com>
2025-11-24 13:00:14 +0100

    Add files via upload

---
+++ src/components/rules-index/index.js
@@ -0,0 +1,6 @@
+export * from "./RulesIndex";
+export * from "./rules-map";
+export * from "./RulesLinksText";
+export * from "./RulesWithIcon";
+export * from "./RuleWithIcon";
+export * from "./LocalizedRuleLink";
